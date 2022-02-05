086a54a67e72170eb644f2b186552844c4e6b4d2
Zakurove <nasser.alharbi001@gmail.com>
2022-02-05 15:11:15 +0300

    Fixing URL

--- science_olympiad/urls.py
+++ science_olympiad/urls.py
@@ -5,7 +5,13 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
+    # url(r'^form/$', views.view_form, name="view_form"),
+    url(r'^(?P<section>\w+)/$', views.view_section, name="view_section"),
+    # url(r'^inventors/register/$', views.add_inventor2, name='add_inventor'),
+    
+
     # url(r'^contest/(?P<contest_id>\d+)/begin/$', views.begin_contest, name='begin_contest'),
+    #
     url(r'^contest/end/$', views.show_contest_endpage, name='end_contest'),
     url(r'^contest/(?P<contest_id>\d+)/walkthrough/$', views.walkthrough_contest, name='handle_walkthrough'),
     url(r'^contest/walkthrough/1/$', views.walkthrough_contest1, name='walkthrough_contest1'),
@@ -16,10 +22,9 @@ urlpatterns = [
     url(r'^contest/(?P<contest_id>\d+)/question/(?P<question_id>\d+)/$', views.show_question, name='show_question'),
 
 
+
     url(r'^timer/$', views.timer, name='timer'),
     # url('hackathon/form/$', TemplateView.as_view(template_name='science_olympiad/hackathon_form.html'), name='hackathon_form'),
     # url('gallery/form/$', TemplateView.as_view(template_name='science_olympiad/gallery_form.html'), name='gallery_form'),
 ]
-
-
 
