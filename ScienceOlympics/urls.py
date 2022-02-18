46f60255d890a54dae4f27113e845112d5d3c45b
Zakurove <nasser.alharbi001@gmail.com>
2022-02-18 17:58:38 +0300

    Changing URL and endpage

--- ScienceOlympics/urls.py
+++ ScienceOlympics/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('', include('science_olympiad.urls')),
-    path('', include(('science_olympiad.urls', 'science_olympiad'), namespace="science_olympiad")),
+    re_path('', include(('science_olympiad.urls', 'science_olympiad'), namespace="science_olympiad")),
     # path('/', include('science_olympiad.urls', namespace="science_olympiad", app_name="science_olympiad")),
 ]
